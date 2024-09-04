1b8bdd75f29ed357811f2c3beff74be90b8863b3
JosueHerreraConvit <convithe@gmail.com>
2024-09-04 01:08:34 -0400

    Primer commit

---
+++ js/Logica.js
@@ -0,0 +1,19 @@
+function typewriterEffect() {
+    const h1Elements = document.querySelectorAll('h1');
+  
+    h1Elements.forEach(h1 => {
+      const text = h1.textContent;
+      h1.textContent = '';
+  
+      let i = 0;
+      const typingInterval = setInterval(() => {
+        h1.textContent += text[i];
+        i++;
+        if (i === text.length) {
+          clearInterval(typingInterval);
+        }
+      }, 50); // Adjust the speed here (lower value = faster typing)
+    });
+  }
+  
+  window.onload = typewriterEffect;
